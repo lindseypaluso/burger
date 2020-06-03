7814c17d87291a6514abbb9c2079a2505046b133
Lindsey Paluso <lindzymarie@gmail.com>
2020-06-02 18:34:12 -0600

    added controllers

---
+++ config/orm.js
@@ -0,0 +1,99 @@
+var connection = require("./connection.js");
+
+// Helper function for SQL syntax.
+// Let's say we want to pass 3 values into the mySQL query.
+// In order to write the query, we need 3 question marks.
+// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
+// In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+
+// selectAll()
+// insertOne()
+// updateOne()
+
+var orm = {
+    selectAll: function (tableInput, callBack) {
+        var queryString = "SELECT * FROM " + tableInput;
+        connection.query(query, (err, res) => {
+            if (err) {
+                throw err
+            };
+            callBack(res)
+
+        })
+    },
+
+    create: function (tableInput, cols, vals, callBack) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            callBack(result);
+        });
+
+    },
+
+    update: function (tableInput, objColVals, condition, callBack) {
+        var queryString = "UPDATE " + tableInput;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            callBack(result);
+        });
+    },
+};
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('Whopper');
+INSERT INTO burgers (name) VALUES ('Big Mac');
+INSERT INTO burgers (name) VALUES ('Peanut Butter Pickle Bacon');
