e57a6f92ff475f7cd6f9de5c50c4c1cf383867aa
Lindsey Paluso <lindzymarie@gmail.com>
2020-07-11 21:29:05 -0600

    seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (name) VALUES ('Whopper');
-INSERT INTO burgers (name) VALUES ('Big Mac');
-INSERT INTO burgers (name) VALUES ('Peanut Butter Pickle Bacon');
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Peanut Butter Pickle Bacon');
